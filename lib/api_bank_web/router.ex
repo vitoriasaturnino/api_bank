6447a5ddfda53e145ced92b1bfa01bf7146be241
vitoriasaturnino <vim.saturnino@gmail.com>
2023-12-21 11:26:37 -0300

    feat: Add route for resource 'accounts'
    This change includes the route 'resources "/accounts", AccountsController' in the routing file. Now, the 'AccountsController' controller is configured to handle CRUD operations related to the 'accounts' resource.

--- lib/api_bank_web/router.ex
+++ lib/api_bank_web/router.ex
@@ -18,6 +18,8 @@ defmodule ApiBankWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+
+    resources "/accounts", AccountsController
   end
 
   # Other scopes may use custom stacks.
