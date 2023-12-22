6ebd32fd41b9525f8848d41995d267d81fcdac50
vitoriasaturnino <vim.saturnino@gmail.com>
2023-12-22 14:18:22 -0300

    refact: improve code readabillity using case statement instead with statement following elixir sintaxe guidelines

--- lib/api_bank_web/controllers/accounts_controller.ex
+++ lib/api_bank_web/controllers/accounts_controller.ex
@@ -8,37 +8,57 @@ defmodule ApiBankWeb.AccountsController do
 
   def index(conn, _params) do
     accounts = Banks.list_accounts()
-    render(conn, :index, accounts: accounts)
+
+    conn
+    |> put_status(:ok)
+    |> render(:index, accounts: accounts)
   end
 
   @spec create(any(), map()) :: any()
   def create(conn, %{"accounts" => accounts_params}) do
-    with {:ok, %Accounts{} = accounts} <- Banks.create_accounts(accounts_params) do
-      conn
-      |> put_status(:created)
-      |> put_resp_header("location", ~p"/api/accounts/#{accounts}")
-      |> render(:show, accounts: accounts)
+    case Banks.create_accounts(accounts_params) do
+      {:ok, %Accounts{} = accounts} ->
+        conn
+        |> put_status(:created)
+        |> put_resp_header("location", ~p"/api/accounts/#{accounts}")
+        |> render(:show, accounts: accounts)
+
+      error ->
+        error
     end
   end
 
   def show(conn, %{"id" => id}) do
     accounts = Banks.get_accounts!(id)
-    render(conn, :show, accounts: accounts)
+
+    conn
+    |> put_status(:ok)
+    |> render(:show, accounts: accounts)
   end
 
   def update(conn, %{"id" => id, "accounts" => accounts_params}) do
     accounts = Banks.get_accounts!(id)
 
-    with {:ok, %Accounts{} = accounts} <- Banks.update_accounts(accounts, accounts_params) do
-      render(conn, :show, accounts: accounts)
+    case Banks.update_accounts(accounts, accounts_params) do
+      {:ok, %Accounts{} = accounts} ->
+        conn
+        |> put_status(:ok)
+        |> render(:show, accounts: accounts)
+
+      error ->
+        error
     end
   end
 
   def delete(conn, %{"id" => id}) do
     accounts = Banks.get_accounts!(id)
 
-    with {:ok, %Accounts{}} <- Banks.delete_accounts(accounts) do
-      send_resp(conn, :no_content, "")
+    case Banks.delete_accounts(accounts) do
+      {:ok, %Accounts{}} ->
+        send_resp(conn, :no_content, "")
+
+      error ->
+        error
     end
   end
 end
